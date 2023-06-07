a54ca6c3fd390a90c29b11c99a781b92a9f2be8f
MucaGomes <contatodevmuca@gamil.com>
2023-06-07 13:27:17 -0300

    first commit about app ViaCEP

---
+++ app/src/main/java/com/app/consultarcep/HttpService.java
@@ -0,0 +1,54 @@
+package com.app.consultarcep;
+
+import static android.app.PendingIntent.getActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Scanner;
+
+import javax.security.auth.login.LoginException;
+
+public class HttpService extends AsyncTask<Void, Void, CEP> {
+    private final String cepInserted;
+    public HttpService(String cep) { this.cepInserted = cep;}
+
+    @Override
+    protected CEP doInBackground(Void... voids) {
+        StringBuilder result = new StringBuilder();
+
+        if (this.cepInserted != null && this.cepInserted.length() == 8) {
+            try {
+                URL url = new URL("https://viacep.com.br/ws/" + this.cepInserted + "/json/");
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Content-type", "application/json");
+                connection.setRequestProperty("Accept", "application/json");
+                connection.setDoOutput(true);
+                connection.setConnectTimeout(5000);
+                connection.connect();
+
+                Scanner scn = new Scanner(url.openStream());
+
+                while (scn.hasNext()){
+                    result.append(scn.next());
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return new Gson().fromJson(result.toString(), CEP.class);
+    }
+}
---
+++ app/src/main/java/com/app/consultarcep/MainActivity.java
@@ -0,0 +1,48 @@
+package com.app.consultarcep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSeachCep;
+    EditText edtCepInserted;
+    TextView txtResult;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSeachCep = findViewById(R.id.btnSearch);
+        edtCepInserted = findViewById(R.id.edtCepInserted);
+        txtResult = findViewById(R.id.txtResult);
+
+        btnSeachCep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String cepInserted = edtCepInserted.getText().toString();
+
+                try {
+                    CEP result = new HttpService(cepInserted.trim()).execute().get();
+                    txtResult.setText(result.toString());
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getBaseContext(),"Digite um CEP Válido!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+}
